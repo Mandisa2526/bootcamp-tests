a30233e2214cb0a121757b57ce2521362dbf5b2d
Mandisa2526 <l.mandisa3@gmail.com>
2023-04-12 11:55:38 +0200

    new

--- findItemsOver.js
+++ findItemsOver.js
@@ -1,5 +1,6 @@
 function findItemsOver(list, min) {
   var items = [];
+  
   for(var i = 0; i < list.length; i++) {
     if (list[i].qty > min) {
       items.push(list[i]);
--- test/findItemsOver.tests.js
+++ test/findItemsOver.tests.js
@@ -8,11 +8,11 @@ describe("Test my findItemsOver function" , function(){
         ];  
         
         var results2 = [];
-
+        
         assert.deepEqual(results2, findItemsOver(itemList2, 20));
     });
 
-    it("it should return a name of, products that have quantity higher than the 20 in the itemList" , function(){
+    it("it should return a name of, products that have quantity higher than the 27 in the itemList" , function(){
         var itemList = [
             {name : 'apples', qty : 10},
             {name : 'pears', qty : 37},
@@ -22,25 +22,23 @@ describe("Test my findItemsOver function" , function(){
         
         var results = [
             {name : 'pears', qty : 37},
-            {name : 'bananas', qty : 27},
+        
         ];
         
-        assert.deepEqual(results, findItemsOver(itemList, 20));
+        assert.deepEqual(results, findItemsOver(itemList, 27));
     });
-    it("it should return a name of, products that have quantity higher than  20 in itemList3" , function(){
+    it("it should return a name of, products that have quantity higher than  30 in itemList3" , function(){
         var results3 = [
             {name : 'apples', qty : 40},
-            {name : 'bananas', qty : 23},
             {name : 'apples', qty : 37}
         ];
         var itemList3 = [
             {name : 'apples', qty : 40},
-            {name : 'pears', qty : 20},
             {name : 'bananas', qty : 23},
             {name : 'apples', qty : 37}
         ];
         
-        assert.deepEqual(results3, findItemsOver(itemList3, 20));
+        assert.deepEqual(results3, findItemsOver(itemList3, 30));
     });
 
 });
