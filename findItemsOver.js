6408ff8fe43b98551152f9f7642e7eb0099d078a
Mandisa2526 <l.mandisa3@gmail.com>
2023-04-12 09:33:34 +0200

    new

--- findItemsOver.js
+++ findItemsOver.js
@@ -1,7 +1,7 @@
 function findItemsOver(list, min) {
   var items = [];
   for(var i = 0; i < list.length; i++) {
-    if (list[i] > min) {
+    if (list[i].qty > min) {
       items.push(list[i]);
     }
   }
--- mostProfitableDepartment.js
+++ mostProfitableDepartment.js
@@ -15,4 +15,20 @@ function mostProfitableDepartment(salesData) {
     }
     return department.department;
   }
-  
+  function mostProfitableDay(salesData) {
+    var days = {};
+    for (var i = 0; i < salesData.length; i++) {
+      if (days[salesData[i].day]) {
+        days[salesData[i].day] += salesData[i].sales;
+      } else {
+        days[salesData[i].day] = salesData[i].sales;
+      }
+    }
+    var day = {day : '', sales : 0};
+    for (var i in days) {
+      if (days[i] >= day.sales) {
+        day = {day : i, sales : days[i]};
+      }
+    }
+    return day.day;
+  }
